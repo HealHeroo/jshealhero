4ac7221b4c1c700fce90c1628ac13c68f78ab9bc
stefanymelda <juwitastefany13@gmail.com>
2023-12-04 11:04:24 +0700

    push function js

--- admin/obat/getFunction/getObatFunction.js
+++ admin/obat/getFunction/getObatFunction.js
@@ -1,4 +1,4 @@
-import { URLGetObat, responseData } from "../js/getObat.js";
+import { URLGetObat, responseData } from "../getFunction/getObat";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { hide } from "https://jscroot.github.io/element/croot.js";
 
